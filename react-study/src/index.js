9f3fc203cee8f29156f5287e92a89646965d037a
vijay <wuweijia2950@gmail.com>
2020-11-24 15:30:04 +0800

    react: api

--- react-study/src/index.js
+++ react-study/src/index.js
@@ -29,24 +29,150 @@ let domContainer = document.getElementById('root')
 // setInterval(tick, 1000);
 
 
-function Welcome(props) {
-  return <h1> hello, { props.name }</h1>
-}
+// function Welcome(props) {
+//   return <h1> hello, { props.name }</h1>
+// }
 
 // const element = <Welcome name="Sara" />
 
-function App () {
-  return (
-    <div>
-      <Welcome name="sara"></Welcome>
-      <Welcome name="cahal"></Welcome>
-      <Welcome name="edite"></Welcome>
-    </div>
-  )
+// function App () {
+//   return (
+//     <div>
+//       <Welcome name="sara"></Welcome>
+//       <Welcome name="cahal"></Welcome>
+//       <Welcome name="edite"></Welcome>
+//     </div>
+//   )
+// }
+
+
+// ReactDOM.render(
+//   <App/>,
+//   domContainer
+// )
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>hello, world</h1>
+//       <h2>it is {props.date.toString()}.</h2>
+//     </div>
+//   )
+// }
+
+// function tick() {
+//   ReactDOM.render(
+//     <Clock date={ new Date() } />,
+//     domContainer
+//   )
+// }
+
+
+// // setInterval(tick, 1000);
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { date: new Date() };
+//   }
+
+//   componentDidMount() {
+//     this.timerID = setInterval(
+//       () => this.tick(),
+//       1000
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
+
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+// }
+
+// class Toggle extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { isToggleOn: true };
+
+//     // 为了在回调中使用 `this`，这个绑定是必不可少的
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.setState(state => ({
+//       isToggleOn: !state.isToggleOn
+//     }));
+//   }
+
+//   render() {
+//     return (
+//       <button onClick={this.handleClick}>
+//         {this.state.isToggleOn ? 'ON' : 'OFF'}
+//       </button>
+//     );
+//   }
+// }
+
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          参与:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          来宾人数:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
 }
 
-
 ReactDOM.render(
-  <App/>,
-  domContainer
-)
+  <Reservation/>,
+  document.getElementById('root')
+);
