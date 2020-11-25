a798333b630eba897ba5384e1d54d38f7ff2e69c
vijay <wuweijia2950@gmail.com>
2020-11-25 11:34:40 +0800

    workflows: init

--- test.js
+++ test.js
@@ -1,53 +1,14 @@
-const state = {
-  count: 0
-}
-let activeUpdate
-
-function Dep() {
-  this.deps = []
-  this.depend = function () {
-    if (activeUpdate) {
-      console.log(activeUpdate);
-      this.deps.push(activeUpdate)
-    }
-  }
-
-  this.notify = function () {
-    this.deps.forEach(dep => dep())
-  }
+function objectFactory() {
+  var obj = {},
+    Constructor = [].shift.call(arguments);
+  obj.__proto__ = Constructor.prototype;
+  var ret = Constructor.apply(obj, arguments);
+  console.log(ret);
+  return typeof ret === 'object' ? ret : obj;
+};
+function fnf() {
+  this.x = 123
 }
 
-const dep = new Dep()
-
-function Observe(obj) {
-  Object.keys(obj).forEach(function(key){
-    let internalValue = obj[key]
-    Object.defineProperty(obj, key, {
-      get() {
-        dep.depend()
-        return internalValue
-      },
-      set(newValue){
-        dep.notify()
-        internalValue = newValue
-      }
-    })
-  })
-}
-
-Observe(state)
-
-function autoRun(update) {
-  function wrappenUpdate() {
-    activeUpdate = wrappenUpdate
-    update()
-    activeUpdate = null
-  }
-  wrappenUpdate()
-}
-
-autoRun(() => {
-  console.log('duqu', state.count);
-})
-
-state.count = 3
+let a2 = objectFactory(fnf) // 模拟 new fnf()
+console.log(a2.x) // 123
