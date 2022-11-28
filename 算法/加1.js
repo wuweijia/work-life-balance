d822492c8ffa0241c136178f43cc8df970550bdb
Rukawa <870007939@qq.com>
2022-11-28 11:20:09 +0800

    Delete 算法 directory

--- 算法/加1.js
+++
@@ -1,7 +0,0 @@
-var plusOne = function (digits) {
-  return (BigInt(digits.join('')) + 1n).toString().split('')
-};
-
-var result = plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3])
-console.log(result);
-
