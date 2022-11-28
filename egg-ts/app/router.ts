d3744b70d479fcf4b486f05481626b4ab37ca910
Rukawa <870007939@qq.com>
2022-11-28 11:18:03 +0800

    Delete egg-ts directory

--- egg-ts/app/router.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from 'egg';
-
-export default (app: Application) => {
-  const { controller, router } = app;
-
-  router.get('/', controller.home.index);
-  router.get('/user', controller.home.user);
-};
