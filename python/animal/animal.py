4693136e45c6df94fc0d0af0568c57abd22d1708
Rukawa <870007939@qq.com>
2022-11-28 11:19:02 +0800

    Delete python directory

--- python/animal/animal.py
+++
@@ -1,9 +0,0 @@
-
-class Animal(object):
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.sex = 0
-
-    def sing(self):
-        print("oh~")
--- python/file/file_reader.py
+++
@@ -1,3 +0,0 @@
-with open('/Users/wuweijia/23mofang/blog/python/file/test.txt') as filecontent:
-    content = filecontent.read()
-    print(content)
